9339ac3a4fd45282e1c8c7635e4dcbd1591da530
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-12-11 21:14:20 +0200

    start project

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,28 @@
-app.controller('HomeController', ['$scope', function($scope) {
-    $scope.helloWorld = "Hello, AngularJS!";
-});
+app.controller('HomeController', ['$scope','suggestionsService', function($scope, suggestionsService) {
+
+    $scope.posts = suggestionsService.posts;
+
+    $scope.addSuggestion = function() {
+        
+           //if input empty, don't submit
+           if(!$scope.title || $scope.title === "") {
+                    return;
+                }
+
+           //push suggestion posts in suggestionsService.js
+           $scope.posts.push({
+                  title: $scope.title,
+                  upvotes: 0
+                });
+
+           //after submit, clear input
+           $scope.title = '';
+           };
+    $scope.upVote = function (index) {
+        $scope.posts[index].upvotes += 1;
+    };
+
+    $scope.comments = posts.comments;
+
+
+}]);
